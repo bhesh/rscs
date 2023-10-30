1326dc5f8636501f255f92aca237e7f0c61a007d
bhesh <github@bhmail.me>
2023-10-30 17:50:50 -0400

    added nothing

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
+#![forbid(unsafe_code)]
+#![warn(
+    clippy::mod_module_files,
+    clippy::unwrap_used,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
+pub fn nothing() {
+    unimplemented!()
+}
