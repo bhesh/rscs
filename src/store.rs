77e25e7fb2ed0edb73d813f1f38ac9c455bf629a
bhesh <github@bhmail.me>
2023-10-31 16:24:46 -0400

    remove PartialEq--Eq implies it

--- src/store.rs
+++ src/store.rs
@@ -8,12 +8,12 @@ use x509_verify::x509_cert::Certificate;
 pub struct CertificateStore<Id, L>(L, PhantomData<Id>)
 where
     L: Loader<Id>,
-    Id: PartialEq + Eq;
+    Id: Eq;
 
 impl<Id, L> CertificateStore<Id, L>
 where
     L: Loader<Id>,
-    Id: PartialEq + Eq,
+    Id: Eq,
 {
     pub fn new() -> Self {
         Self(L::default(), PhantomData::default())
