d2c9dd6d8f6c309e2488e3f75b1216ed53e2ed02
bhesh <github@bhmail.me>
2023-10-31 18:25:40 -0400

    verification

--- src/store.rs
+++ src/store.rs
@@ -15,7 +15,6 @@ pub struct CertificateStore<Id, L>
 where
     L: Loader<Id>,
     Id: Eq + for<'a> TryFrom<&'a Certificate>,
-    Error: for<'a> From<<Id as TryFrom<&'a Certificate>>::Error>,
 {
     inner: L,
     phantom: PhantomData<Id>,
@@ -25,7 +24,6 @@ impl<Id, L> CertificateStore<Id, L>
 where
     L: Loader<Id>,
     Id: Eq + for<'a> TryFrom<&'a Certificate>,
-    Error: for<'a> From<<Id as TryFrom<&'a Certificate>>::Error>,
 {
     /// Creates an empty [`CertificateStore`]
     pub fn new() -> Self {
@@ -35,9 +33,12 @@ where
         }
     }
 
-    /// Inserts a certificate into the certificate store. Returns [`Error`] if the conversion from
+    /// Inserts a certificate into the certificate store. Returns `E` if the conversion from
     /// `Certificate` to `Id` fails.
-    pub fn insert(&mut self, cert: Certificate) -> Result<Option<Certificate>, Error> {
+    pub fn insert<E>(&mut self, cert: Certificate) -> Result<Option<Certificate>, E>
+    where
+        E: for<'a> From<<Id as TryFrom<&'a Certificate>>::Error>,
+    {
         let id = Id::try_from(&cert)?;
         Ok(self.inner.insert(id, cert))
     }
@@ -56,6 +57,25 @@ where
     /// Returns an iterator over the internal storage in the form of a Tuple `(&Id, &Certificate)`
     pub fn iter(&self) -> Box<dyn Iterator<Item = (&'_ Id, &'_ Certificate)> + '_> {
         self.inner.iter()
+    }
+
+    /// Verifies `cert` up its own issuance chain. Verification ends once a certificate in `cert`'s
+    /// chain is found in the certificate store and all signatures are verified. Verification fails
+    /// when any of the following hold true:
+    ///
+    /// - None of the certificates in the chain are trusted
+    /// - The trust chain is broken and can not be inferred
+    /// - Any of the certificates in the trust chain are expired
+    /// - Any of the certificates in the trust chain are not yet valid
+    /// - Signature verification fails
+    ///
+    /// This function makes no guarantees about the certificates in `chain`. The sole focus is to
+    /// verify `cert` all the way up its own trust chain. The `chain` is only used when an issuer
+    /// in `cert`'s trust chain is not already present in the certificate store.
+    ///
+    /// This function does not provide any revocation checking.
+    pub fn verify(&self, cert: &Certificate, chain: &[&Certificate]) -> Result<(), Error> {
+        unimplemented!()
     }
 }
 
